5feca504c212969888538c76c93f7643f964327e
HipLiv <96388870+HipLiv@users.noreply.github.com>
2022-01-24 20:32:17 +0100

    Added a changing weather icon

--- src/script.js
+++ src/script.js
@@ -27,6 +27,11 @@ function showWeather(response) {
   document.querySelector("#currentTemperature").innerHTML = Math.round(
     response.data.main.temp
   );
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
 }
 
 function search(city) {
